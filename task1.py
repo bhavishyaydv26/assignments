d3e44ef24a355e769fa19af0f1a0811de06e3e33
bhavishyaydv26 <bhavishyaahir345@gmail.com>
2025-11-09 10:56:06 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("The sum of numbers is:",addition)
+print("The difference of numbers is:",subtraction)
+print("The multipication of numbers id:",multiplication)
+print("The division of numbers is:",division)
---
+++ task2.py
@@ -0,0 +1,3 @@
+Name=input("Enter your first Name:")
+Lastname=input("Enter your last Name:")
+print("Hello,",Name,Lastname,"!","Welcome to the python program")
